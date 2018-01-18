33cfc60a621ab2ef6a1bc794036505acb66ed484
Renan Roberto <renanroberto1@gmail.com>
2018-01-18 11:42:32 -0200

    change page title; add clear filter function; change favicon.

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -40,6 +40,47 @@ class Filters extends Component {
     }
   }
 
+  clearFilter = () => {
+    const colors = [
+      { label: 'Vermelho', active: false },
+      { label: 'Azul', active: false },
+      { label: 'Branco', active: false },
+      { label: 'Preto', active: false },
+      { label: 'Laranja', active: false },
+    ]
+
+    const sizes = [
+      { label: 'p', active: false },
+      { label: 'm', active: false },
+      { label: 'g', active: false },
+      { label: 'gg', active: false },
+      { label: 'u', active: false },
+      { label: '36', active: false },
+      { label: '38', active: false },
+      { label: '40', active: false },
+      { label: '42', active: false },
+      { label: '44', active: false },
+      { label: '46', active: false }
+    ]
+
+    const prices = [
+      { label: 'de R$ 0 até R$ 50', active: false },
+      { label: 'de R$ 51 até R$ 150', active: false },
+      { label: 'de R$ 151 até R$ 300', active: false },
+      { label: 'de R$ 301 até R$ 500', active: false },
+      { label: 'a partir de R$ 501', active: false },
+    ]
+
+    const filters = {
+      colors: [],
+      sizes: [],
+      price: ''
+    }
+
+    this.props.update(filters)
+    this.setState({ colors, sizes, prices, filters })
+  }
+
   colorHandler = (index) => {
     const colors = [...this.state.colors]
     colors[index].active = !colors[index].active
@@ -125,6 +166,8 @@ class Filters extends Component {
           <h2>Faixa de preço</h2>
           {prices}
         </div>
+
+        <div onClick={this.clearFilter} className="clear">Limpar filtros</div>
       </div>
     )
   }
